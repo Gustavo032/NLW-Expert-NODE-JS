ae2a2f2acf35536783fd29404c46e9e9c749c2d3
Gustavo032 <gustavoramosrock@gmail.com>
2024-02-08 05:42:42 -0300

    aula 2 finished

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,24 +1,21 @@
 import fastify from 'fastify'
 import { z } from 'zod'
 import { prisma } from '../lib/prisma'
+import { createPoll } from '../routes/create-poll'
+import { getPoll } from '../routes/get-poll'
+import { voteOnPoll } from '../routes/vote-on-poll'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
-app.post('/polls', async (request, reply) => {
-	const createPollBody = z.object({
-		title: z.string()
-	})
-
-	const { title } = createPollBody.parse(request.body)
-
-	const poll = await prisma.poll.create({
-		data:{
-			title
-		}
-	})
-
-	return reply.status(201).send({ pollId: poll.id})
+app.register(cookie,{
+	secret: "pokas´-=[xcm90=l1´po=l;234.=-1l;=2[~e/", // for cookies signature
+  hook: 'onRequest', // set to false to disable cookie autoparsing or set autoparsing on any of the following hooks: 'onRequest', 'preParsing', 'preHandler', 'preValidation'. default: 'onRequest'
 })
+
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({port: 3333}).then(() => {
 	console.log('listening on port 3333')
